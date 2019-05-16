e9403f2924f6703477a558b89f575161eaab7529
Sebastian Junca <sebastianjunca@Sebastians-MacBook-Pro.local>
2019-05-16 20:04:35 +0200

    Prueba de commit de BootCamp Keep Coding stop cancel

---
+++ maxminlen.py
@@ -0,0 +1,54 @@
+def maxi(*l):
+    if len(l) == 0:
+        return 0
+   
+   m = l[0]
+   
+    for lx in range(1, len(l)):
+        if l[ix] > m:
+            m = l[ìx]
+    
+    return m
+
+# Compara uno a uno los números en el array y va grabando en m el mayor. compara el segundo con el primero, si es mayor graba en m, sino sigue con el tercero.
+
+
+def media(*l):
+	if len(l) == 0:
+		return 0
+	suma = 0
+	for valor in l:
+		suma += valor
+	return suma / len(l)
+
+
+    
+ def mini(*l):
+ 		if len(l) == 0:
+ 			return 0
+ 			
+ 		m = l[0]
+ 		
+ 		for ix in range(1. len(l)):
+ 			if l[ix] < m:
+ 				m = l[ix]
+ 				
+ 			return m
+ 			
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+
+
+    
